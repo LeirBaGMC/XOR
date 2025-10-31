7bf0ee41303e40d8a8082192737e924cb15d3f63
Gabriel Minda <gabominda@outlook.com>
2025-10-31 12:38:41 -0500

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -2,13 +2,13 @@ def xor_encryption(text, key):
     encrypted_text = ""
     
     for i in range(len(text)):
-        encrypted_text += chr(ord(text[i]) ^ ord(key[i]))
+        encrypted_text += chr(ord(text[i]) ^ ord(key[i]))  
     
     return encrypted_text
 
 
 plain_text = "Educative Accelerates Developer Productivity"
-key = "Edu_key"
+key = "Educative Accelerates Developer Productivity"  
 
 encrypted_text = xor_encryption(plain_text, key)
 print(f'Encrypted Text: {encrypted_text}')
